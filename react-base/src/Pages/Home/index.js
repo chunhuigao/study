6fb618a370c1bd08ccc163677315b800d0c48c86
高春辉 <gaochunhui@gaochunhuideMacBook-Pro.local>
2020-06-29 07:49:47 +0800

    添加抽屉组建

--- react-base/src/Pages/Home/index.js
+++ react-base/src/Pages/Home/index.js
@@ -1,5 +1,27 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
+import { Menu, Button, Drawer } from 'antd'
 
 export default () => {
-  return <div>我是home页面</div>
+  const [visible, setVisible] = useState(true)
+  const [collapsed, setCollapsed] = useState(true)
+  const onClose = () => {
+    setVisible(false)
+  }
+  const toggleCollapsed = () => {}
+  return (
+    <div>
+      <h2>我是home页面</h2>
+      <Drawer
+        title="Basic Drawer"
+        placement="left"
+        closable={false}
+        onClose={onClose}
+        visible={visible}
+      >
+        <p onClick={onClose}>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Drawer>
+    </div>
+  )
 }
--- react-base/src/routers/index.js
+++ react-base/src/routers/index.js
@@ -1,4 +1,4 @@
-import React, { lazy } from 'react'
+import React, { lazy, Suspense } from 'react'
 import { HashRouter, Route, Switch } from 'react-router-dom'
 // import Home from '../Pages/Home/index'
 // import Details from '../Pages/Details/index'
@@ -7,12 +7,14 @@ const Home = lazy(() => import('../Pages/Home/index'))
 const Details = lazy(() => import('../Pages/Details/index'))
 const RouterConfig = () => {
   return (
-    <HashRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/details" component={Details} />
-      </Switch>
-    </HashRouter>
+    <Suspense fallback={<div>Loading</div>}>
+      <HashRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/details" component={Details} />
+        </Switch>
+      </HashRouter>
+    </Suspense>
   )
 }
 export default RouterConfig
